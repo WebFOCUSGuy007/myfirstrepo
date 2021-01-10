4e0aa7a2c03125b6bcc7e21f421470d9e16ede29
WebFOCUSGuy <dbriars@nfa.futures.org>
2021-01-10 16:35:02 -0600

    Updated application on Jan 10 2021

---
+++ DaveNJohn/ClientApp/src/components/mycomponent.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+export class mycomponent extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+
+            </div>
+        );
+    }
+
+
+}
